d69233e22b46c1748eb403b609acc43705a1844c
Tim Strazzere <Tim.Strazzere@mylookout.com>
2013-09-16 12:50:33 -0700

    Cleaned up directives regex.
    Things with the word 'local' where not being parsed properly due to
    bad regexing. Cleaned up the regex to not be so ugly and also fixed
    some spacing issues.

--- smali-mode.el
+++ smali-mode.el
@@ -97,7 +97,7 @@
     )
     ;; directives
     (
-     ".locals\\|.local\\|.end\s+\\(field\\|subannotation\\|annotation\\|method\\|array-data\\)\\|\.end\s+\\(packed-switch\\|sparse-switch\\|parameter\\|local\s\\)\\|.class\\|.super\\|.proto\\|.implements\\|.field\\|.subannotation\\|.annotation\\|.enum\\|.method\\|.registers\s+[0-9]+\\|.array-data\\|.packed-switch\\|.sparse-switch\\|.end\s+\\(local\\)\\|.parameter\\|.prologue\\|.epilogue\\|.source\\|\.restart\s+local" . font-lock-builtin-face
+     ".locals|\\.local\\|.end\s\\(field\\|subannotation\\|annotation\\|method\\|array-data\\|packed-switch\\|sparse-switch\\|parameter\\|local\\)\\|.class\\|.super\\|.proto\\|.implements\\|.field\\|.subannotation\\|.annotation\\|.enum\\|.method\\|.registers\s+[0-9]+\\|.array-data\\|.packed-switch\\|.sparse-switch\\|.parameter\\|.prologue\\|.epilogue\\|.source\\|.restart\slocal" . font-lock-builtin-face
     )
     ;; tags
     (
@@ -105,7 +105,7 @@
     )
     ;; annotations
     (
-     "accessFlags \\|name \\|value \\|system " . font-lock-variable-name-face
+     "accessFlags\s\\|name\s\\|values\s\\|system\s" . font-lock-variable-name-face
     )
     ;; access
     (

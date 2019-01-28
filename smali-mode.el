24d3ac419d0cc4822ea53816bb2f5a115cdee5f3
Tim Strazzere <strazz@gmail.com>
2019-01-28 10:14:56 -0800

    update header and add license

--- smali-mode.el
+++ smali-mode.el
@@ -1,10 +1,23 @@
 ;;; smali-mode.el --- Major mode for editing Smali/Baksmali files
 ;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
 ;; Filename: smali-mode.el
 ;; Description:
-;; Author: Tim Strazzere <strazz@gmail.com> <diff@lookout.com>
+;; Author: Tim Strazzere <strazz@gmail.com> <diff@protonmail.com>
 ;; Maintainer:
-;; Copyright (C) 2015, Tim Strazzere, all rights reserved.
+;; Copyright (C) 2015-219, Tim Strazzere, all rights reserved.
 ;; Created:
 ;; Version:
 ;; Last-Updated:

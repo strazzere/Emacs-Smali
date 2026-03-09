59735e81ccdcf94923e8825489c9d3e060c443fe
Hong Xu <hong@topbug.net>
2026-03-08 18:41:55 -0700

    Use lexical binding
    Eliminating the warning when byte-compile:
    
    smali-mode.el:1:1: Warning: file has no ‘lexical-binding’ directive on its first line

--- smali-mode.el
+++ smali-mode.el
@@ -1,4 +1,4 @@
-;;; smali-mode.el --- Major mode for editing Smali/Baksmali files
+;;; smali-mode.el --- Major mode for editing Smali/Baksmali files  -*- lexical-binding: t; -*-
 ;;
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
